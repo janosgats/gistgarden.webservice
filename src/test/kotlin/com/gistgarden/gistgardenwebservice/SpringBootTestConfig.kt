f7732163e86d5742f0c6af1e56d07a211f3ee91c
gjani <gatsjanos@gmail.com>
2024-02-06 03:09:32 +0100

    Fixing tests

--- src/test/kotlin/com/gistgarden/gistgardenwebservice/SpringBootTestConfig.kt
+++ src/test/kotlin/com/gistgarden/gistgardenwebservice/SpringBootTestConfig.kt
@@ -4,11 +4,17 @@ import com.zaxxer.hikari.HikariDataSource
 import org.mockito.Mockito
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.session.jdbc.JdbcIndexedSessionRepository
 
 @TestConfiguration
 class SpringBootTestConfig {
     @Bean
-    fun dtaSource(): HikariDataSource {
+    fun dataSource(): HikariDataSource {
         return Mockito.mock(HikariDataSource::class.java)
+    }
+
+    @Bean
+    fun jdbcIndexedSessionRepository(): JdbcIndexedSessionRepository {
+        return Mockito.mock(JdbcIndexedSessionRepository::class.java)
     }
 }
