703a4ed739a8146b7b92953c62662c2be35f9b59
gjani <gatsjanos@gmail.com>
2024-02-23 22:55:03 +0100

    Fixing tests

--- src/test/kotlin/com/gistgarden/gistgardenwebservice/ApplicationTests.kt
+++ src/test/kotlin/com/gistgarden/gistgardenwebservice/ApplicationTests.kt
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("test")
 @SpringBootTest(classes = [SpringBootTestConfig::class], properties = ["spring.main.allow-bean-definition-overriding=true"])
 class ApplicationTests {
 
